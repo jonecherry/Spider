0304a9bea11bf72b7f655785355511484fbaf625
jonecherry <junchen.unif@qq.com>
2016-05-27 23:21:03 +0800

    m

--- gufensoso/yinyong.py
+++ gufensoso/yinyong.py
@@ -9,7 +9,7 @@ def getsource(url):
     return html.text
 
 if __name__ == '__main__':
-    url = 'http://gg3.cytbj.com/scholar?q=社会网络'
+    url = 'http://gg3.cytbj.com/scholar?q=社会网络动态分析'
     html = getsource(url)
     selector = etree.HTML(html)
     referenceNum = selector.xpath('//div[@class="gs_fl"]/a/text()')[0]
