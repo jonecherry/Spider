2b4e42a3f24ad11eaf7fd41535d8cb57ed347390
jonecherry <junchen.unif@qq.com>
2016-05-28 09:28:07 +0800

    处理特殊抓取

--- mafengwo.py
+++ mafengwo.py
@@ -19,46 +19,48 @@ if __name__ == '__main__':
     if not os.path.exists(os.path.join('zhuaqu','伦敦')):
         os.mkdir(os.path.join('zhuaqu','伦敦'))
     jilu = open(os.path.join('zhuaqu','伦敦','lundun.txt'),'a')
+    londonPOI = open('londonPOI.txt','a')
 
     for i in range(2,3):
         url = 'http://www.mafengwo.cn/group/s.php?q=伦敦&p='+str(i)+'&t=poi'
         html = getsource(url)
-
-        # html = unicode(html, "utf-8")
-
         selector = etree.HTML(html)
-        # print html
-        print 'html类型'
-        print type(html)
+        # print 'html类型'
+        # print type(html)
 
         jilu.write(html)
         jilu.close()
-        poilist = selector.xpath('//div[@class="ct-text "]/h3/a/text()')
+        # poilist = selector.xpath('//div[@class="ct-text "]/h3/a/text()')
+        poilist = selector.xpath('//div[@class="ct-text "]/h3')
         for poi in poilist:
-            print '类型-中文名称 英文名称'
-            print poi
-            print
+            poi = poi.xpath('string(.)')
+            poi = poi.replace('\n','')
+            print '类型 - 中文名称 英文名称'
+            print poi.replace(' ','')
             elements = poi.split('-')
             leixin = elements[0]
             print '类型:'
+            leixin = leixin.replace(' ','')
             print leixin
             temp = elements[1].split()
-            zhongwen = temp[0]
+            if len(temp)==1:
+                zhongwen=temp[0]
+                yingwen=temp[0]
+            else:
+                zhongwen = temp[0]
+                yingwen = ''
+                for i in range(1,len(temp)):
+                    yingwen = yingwen +' '+ temp[i]
+
             print '中文:'
             print zhongwen
-            yingwen = ''
-            for i in range(1,len(temp)):
-                yingwen = yingwen +' '+ temp[i]
             print '英文:'
             print yingwen
-            mingcheng = elements[1]
-            print 'poi:'
-            print mingcheng
-            print '--------------------'
-            break
-            # poi = unicode(poi,"utf-8")
-            # print poi.encode('gbk')
-            # print poi
-            # print poi.decode('
 
+            line = '伦敦'+','+leixin+','+zhongwen+','+yingwen+'\n'
+            print '收录poi:'
+            print line
+            londonPOI.write(line)
+            print '--------------------'
+    londonPOI.close()
     print 'finished'
